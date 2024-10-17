7f72af21bb37e2343c8ed300cfc24065f05ac964
jeyli <73306164+jeyli666@users.noreply.github.com>
2024-10-16 18:29:54 -0700

    práctica 9 corregida 16/10/2024

--- BaseDeDatosWeb/Parcial2/practica9/practica9.js
+++ BaseDeDatosWeb/Parcial2/practica9/practica9.js
@@ -15,10 +15,38 @@ $(document).ready( function(){
                 b=false;
                 cont++;
             }
-            if(cont==54){
+            if(cont == 54){
                 console.log("Ya se dieron todas las cartas");
                 b=false;
             }
         }
      });
+     cargacartas();
+
+    function cargacartas(){
+        var carta1 = 0;
+        var carta2 = 0;
+        let asignadas = new Array(54).fill(false);
+
+        while(carta1 < 16){
+            var num = Math.floor(Math.random()*54)+1;
+            if(asignadas[num-1] == false){
+                $("#carta1").append("<div class='col-sm-3'><img class='img-fluid' src='loteria/"+num+".jpg'></div>");
+                asignadas[num-1] = true;
+                carta1++;
+            }else{
+                continue;
+            }
+        }
+        while(carta2 < 16){
+            var num = Math.floor(Math.random()*54)+1;
+            if(asignadas[num-1] == false){
+                $("#carta1").append("<div class='col-sm-3'><img class='img-fluid' src='loteria/"+num+".jpg'></div>");
+                asignadas[num-1] = true;
+                carta2++;
+            }else{
+                continue;
+            }
+        }
+    }
 });
